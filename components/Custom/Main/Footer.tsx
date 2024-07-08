108ae8e2a6e294058fbd01a4888190c66ac6d367
Sanjaee <afrizaahmad18@gmail.com>
2024-07-08 08:14:36 +0700

    first commit

--- components/Custom/Main/Footer.tsx
+++ components/Custom/Main/Footer.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import {
   FaLinkedin,
   FaGithub,
@@ -6,22 +7,36 @@ import {
   FaTiktok,
   FaYoutube,
 } from "react-icons/fa";
+import PasswordModal from "./PasswordModal"; // Adjust the import path if necessary
 
 const Footer = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleLinkedInClick = () => {
+    setModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setModalOpen(false);
+  };
+
+  const handleModalSubmit = () => {
+    setModalOpen(false);
+    window.location.href = "https://www.linkedin.com/in/ahmad-afriza-ab9173276";
+  };
+
   return (
     <footer className="flex w-full relative flex-col items-center p-4 bg-gray-800 text-white">
       <div>
         <p className="text-sm mb-6">&copy; 2024 Ezadev. All rights reserved.</p>
       </div>
       <div className="flex justify-center items-center mb-4">
-        <a
-          href="https://www.linkedin.com/in/ahmad-afriza-ab9173276"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          onClick={handleLinkedInClick}
           className="mx-4 hover:text-gray-400"
         >
           <FaLinkedin size={30} />
-        </a>
+        </button>
         <a
           href="https://github.com/Sanjaee"
           target="_blank"
@@ -55,6 +70,12 @@ const Footer = () => {
           <FaYoutube size={30} />
         </a>
       </div>
+      {isModalOpen && (
+        <PasswordModal
+          onClose={handleModalClose}
+          onSubmit={handleModalSubmit}
+        />
+      )}
     </footer>
   );
 };
