c94a3912812764e9b2eb5b38978d13e2289408e6
Sanjaee <afrizaahmad18@gmail.com>
2026-06-06 06:41:31 +0700

    dfg

--- components/Custom/AboutComponents/AuthorLayout.tsx
+++ components/Custom/AboutComponents/AuthorLayout.tsx
@@ -29,7 +29,7 @@ export default function AuthorLayout({ children }: any) {
                   alt="Ahmad Afriza"
                   height={130}
                   width={130}
-                  src="/profile.png"
+                  src="/avatar.jpg"
                   className="rounded-full object-scale-down "
                 />
               </div>
